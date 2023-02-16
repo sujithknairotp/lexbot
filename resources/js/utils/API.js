4ae75d09e1fc7a1d5a1e8507ebbba699719459fd
“arjun.v” <arjun.v@cubettech.com>
2023-02-16 17:39:03 +0530

    updated build

--- resources/js/utils/API.js
+++ resources/js/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "http://54.237.127.131/api",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
